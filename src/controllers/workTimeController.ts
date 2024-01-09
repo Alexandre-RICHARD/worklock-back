a44807e2214a02702ed6cd73bf9a3b40d0719613
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2024-01-09 17:24:49 +0100

    :building_construction: Changes controller name and add all routes to handle

---
+++ src/controllers/workTimeController.ts
@@ -0,0 +1,125 @@
+import {Request, Response} from "express";
+
+// import {worktimeModel} from "./../models/worktime";
+
+const mainController = {
+    "getAllWorkTime": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "getAllProjects": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "addOneProject": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "changeOneProject": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "deleteOneProject": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "getProjectTasks": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "addOneTasks": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "modifyOneTasks": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "deleteOneTasks": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "getProjectWorkTime": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "addOnetWorkTime": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "modifyOnetWorkTime": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+    "deleteOnetWorkTime": async (_req: Request, res: Response) => {
+        try {
+            // CODE HERE
+            res.json("LOL");
+        } catch (error) {
+            console.trace(error);
+            res.status(200).json(error);
+        }
+    },
+};
+
+export default mainController;
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,25 @@
 import {Request, Response} from "express";
 import {Router as createRouter} from "express";
-import mainController from "./controllers/mainController";
+import workTimeController from "./controllers/workTimeController";
 const router = createRouter();
 
 // Here will be all our routes
-router.get("/alltimework", mainController.getAllTimeWork);
+router.get("/worktime/all", workTimeController.getAllWorkTime);
+
+router.get("/project/getall", workTimeController.getAllProjects);
+router.post("/project/add", workTimeController.addOneProject);
+router.patch("/project/modify", workTimeController.changeOneProject);
+router.delete("/project/delete", workTimeController.deleteOneProject);
+
+router.get("/project/gettasks", workTimeController.getProjectTasks);
+router.post("/tasks/add", workTimeController.addOneTasks);
+router.patch("/tasks/modify", workTimeController.modifyOneTasks);
+router.delete("/tasks/delete", workTimeController.deleteOneTasks);
+
+router.get("/project/getworktime", workTimeController.getProjectWorkTime);
+router.post("/worktime/add", workTimeController.addOnetWorkTime);
+router.patch("/worktime/modify", workTimeController.modifyOnetWorkTime);
+router.delete("/worktime/delete", workTimeController.deleteOnetWorkTime);
 
 // Handling all other route unassigned to a controller method
 router.use((_req: Request, res: Response): void => {
