fe8ce5f7008300f1601f4fa857a66a924b77136f
Alexandre RICHARD <alexandre.richard.dev@gmail.com>
2024-01-18 13:26:46 +0100

    :rocket: Upgrade .env files to make them more understandable

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -4,11 +4,14 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             LOCAL_PORT: number;
+            CORS_ORIGIN: string;
             DB_HOST: string;
             DB_PORT: number;
             DB_USER: string;
             DB_PASSWORD: string;
             DB_DATABASE: string;
+            PROJECT_NAME: string;
+            ERROR_API_URL: string;
         }
     }
 }
